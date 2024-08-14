7c85fded2b2163dd19a161c4eb60b81bd9e1765c
alena <danteoushen@yandex.ru>
2024-08-14 12:20:46 +0300

    Сдача задания

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from main import BooksCollector
+import pytest
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -18,7 +19,64 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert collector.books_genre == {'Гордость и предубеждение и зомби': '', 'Что делать, если ваш кот хочет вас убить': ''}
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+#2
+
+    def test_set_book_genre_check_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
+        assert collector.books_genre == {'Гордость и предубеждение и зомби': 'Ужасы'}
+
+#3
+    @pytest.mark.parametrize('book,genre', [['Гордость и предубеждение и зомби', 'Ужасы'],['Что делать, если ваш кот хочет вас убить', 'Фантастика']])
+    def test_get_book_genre_get_genre(self, book, genre):
+        collector = BooksCollector()
+        collector.books_genre = {book: genre}
+        assert collector.get_book_genre(book) == genre
+
+#4
+
+    def test_get_books_with_specific_genre_fantasy_genre(self):
+        collector = BooksCollector()
+        collector.books_genre = {'Что делать, если ваш кот хочет вас убить': 'Фантастика'}
+        assert collector.get_books_with_specific_genre('Фантастика') == ['Что делать, если ваш кот хочет вас убить']
+
+#5
+
+    def test_get_books_genre_get_books_dictionary(self):
+        collector = BooksCollector()
+        collector.books_genre = {'Гордость и предубеждение и зомби': 'Ужасы'}
+        assert collector.get_books_genre() == {'Гордость и предубеждение и зомби': 'Ужасы'}
+
+#6
+
+    def test_get_books_for_children_genre_for_children(self):
+        collector = BooksCollector()
+        collector.books_genre = {'Зверополис': 'Мультфильмы', 'Гордость и предубеждение и зомби': 'Ужасы'}
+        assert collector.get_books_for_children() == ['Зверополис']
+
+#7
+
+    def test_dd_book_in_favorites_add_one_book(self):
+        collector = BooksCollector()
+        collector.books_genre = {'Зверополис': 'Мультфильмы'}
+        collector.add_book_in_favorites('Зверополис')
+        assert collector.favorites == ['Зверополис']
+
+#8
+
+    def test_delete_book_from_favorites_delete_one_book(self):
+        collector = BooksCollector()
+        collector.favorites == ['Зверополис']
+        collector.delete_book_from_favorites('Зверополис')
+        assert collector.favorites == []
+
+#9
+
+    def test_get_list_of_favorites_books_get_list_wth_one_book(self):
+        collector = BooksCollector()
+        collector.favorites = ['Зверополис']
+        assert collector.get_list_of_favorites_books() == ['Зверополис']
